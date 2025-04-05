be2b40cd15f85888d94a1e51c7cb2896ba83f80a
kareenap55 <kareenap1412@gmail.com>
2025-04-05 08:50:25 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+<script>
+  function removeSelectedColor() {
+    const select = document.getElementById('colorSelect');
+    const selectedIndex = select.selectedIndex;
+
+    if (selectedIndex !== -1) {
+      select.remove(selectedIndex);
+    }
+  }
+</script>
